4249ecbe442e2d5852590de8046a471fefb0c032
Crafterchen2 <Decker_ben@outlook.de>
2026-03-30 00:22:00 +0200

    added menu button enum

--- launchpad/ButtonIndex.cs
+++ launchpad/ButtonIndex.cs
@@ -15,6 +15,37 @@ public readonly struct ButtonIndex : IByteTransmittable
 
     public static implicit operator byte(ButtonIndex i) => i.Index;
     public static implicit operator ButtonIndex(byte i) => new(i);
+    public static implicit operator ButtonIndex(MenuButtonIndex i) => new((byte)i);
+    public static explicit operator MenuButtonIndex(ButtonIndex i) => (MenuButtonIndex)i.Index;
 
     public byte[] ToBytes() => [Index];
+
+    public bool IsMenuButton => Index != 99 && (X == 9 || Y == 9);
+    public bool IsStatusLed => Index == 99;
+}
+
+public enum MenuButtonIndex : byte
+{
+    ArrowUp = 91,
+    ArrowDown = 92,
+    ArrowLeft = 93,
+    ArrowRight = 94,
+    SessionMixer = 95,
+    Note = 96,
+    Custom = 97,
+    Capture = 98,
+    StatusLed = 99,
+    PageVolume = 89,
+    PagePan = 79,
+    PageSendA = 69,
+    PageSendB = 59,
+    PageStopClip = 49,
+    PageMute = 39,
+    PageSolo = 29,
+    PageRecordArm = 19
+}
+
+public static class MenuButtonIndexExtension
+{
+    public static ButtonIndex ToStruct(this MenuButtonIndex i) => new((byte)i);
 }
