0ad84a1d9ca372d5ca0c036590d9a92657e13a3e
Crafterchen2 <decker_ben@outlook.de>
2026-07-04 22:55:47 +0200

    Added + and - operator

--- UInt7.cs
+++ UInt7.cs
@@ -12,6 +12,14 @@ public readonly struct UInt7 : IByteTransmittable
         Value = b;
     }
 
+    public static UInt7 operator +(UInt7 a, UInt7 b) => new((byte)((a.Value + b.Value) % 127));
+    public static UInt7 operator -(UInt7 a, UInt7 b)
+    {
+        var d = a.Value - b.Value;
+        if (d < 0) d += 127;
+        return new UInt7((byte)(d % 127));
+    }
+
     public static implicit operator byte(UInt7 b) => b.Value;
     public static implicit operator UInt7(byte b) => new(b);
 
